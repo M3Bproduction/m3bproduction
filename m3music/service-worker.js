507d3c1c6c933dd2fdeac94c9eb76750f0b9c28c
M3Bproduction <madjidchakira@gmail.com>
2025-07-01 00:36:53 +0300

    Delete service-worker.js

--- m3music/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installé');
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
