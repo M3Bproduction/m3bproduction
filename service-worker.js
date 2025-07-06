23b4571f1bcaa115eb6cf113fc7660c0171052bb
M3Bproduction <madjidchakira@gmail.com>
2025-07-06 19:43:30 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,42 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installé');
-  self.skipWaiting();
+const CACHE_NAME = 'm3b-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/m3b-production.png',
+  '/favicon.ico',
+  '/manifest.json',
+  // Ajoute ici les autres fichiers que tu veux rendre accessibles hors ligne
+  '/m3music/music.html',
+  '/m3info/info.html',
+  // etc.
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => 
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
 });
